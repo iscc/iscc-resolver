613f00e9f86f892f74a492b1b295d3909da72f28
Titusz Pan <titusz.pan@gmail.com>
2021-05-30 17:46:14 +0200

    Tweak start page.

--- isccr/core/views.py
+++ isccr/core/views.py
@@ -8,17 +8,28 @@ from isccr.core.models import IsccID
 
 def index(request):
     html = """
-    <!doctype html>
-    <html lang="en">
-      <head>
-        <meta charset="utf-8">
-        <title>ISCC Resolver</title>
-      </head>
-      <body>
-        <p>try /<iscc-id></p>
-      </body>
-    </html>
-    """
+    <!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hello Bulma!</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css">
+  </head>
+  <body>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">
+        ISCC MetaRegistry - MVP
+      </h1>
+      <p class="subtitle">
+        Try https://iscc.in/&lt;iscc-id&gt; or <a href="/browse/">/browse</a> ISCC Short-IDs
+      </p>
+    </div>
+  </section>
+  </body>
+</html>
+"""
     return HttpResponse(html)
 
 
